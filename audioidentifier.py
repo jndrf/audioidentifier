18c16883bab0068f9637fd664f7ea66e8a1615ff
Jonas Neundorf <jneun1@web.de>
2022-03-29 22:11:28 +0200

    modify tags of ogg and mp3 files, picker function for tags

--- audioidentifier.py
+++ audioidentifier.py
@@ -4,17 +4,20 @@ import asyncio
 import argparse
 import os
 import subprocess
+import sys
 import tempfile
 import time
 
 import json
+from mutagen.easyid3 import EasyID3
+from mutagen.oggvorbis import OggVorbis
 import requests
 
 API_URL = 'https://audiotag.info/api'
 
 def truncate_audio_file(inputpath, duration='00:20'):
     '''return path to temp file with first duration mm:ss'''
-    handle, temppath = tempfile.mkstemp(prefix='audioidentifier', suffix='.wav')
+    handle, temppath = tempfile.mkstemp(prefix='audioidentifier', suffix='.ogg')
     os.close(handle)
     subprocess.run(['sox', inputpath, temppath, 'trim', '0', duration], check=True)
     return temppath
@@ -41,15 +44,62 @@ def retrieve_result(token, apikey):
             raise RuntimeError(reply['error'])
         if reply['result'] == 'wait':
             time.sleep(1)
+        elif reply['result'] == 'found':
+            return reply['data']
         else:
             break
 
-    
+def apply_tags_mp3(path, **kwargs): #title='', artist='', album='', year=''):
+    id3tags = EasyID3(path)
+    for tag, value in kwargs.items():
+        if not tag in EasyID3.valid_keys.keys():
+            continue
+        id3tags[tag] = value
+
+    id3tags.save()
+
+def apply_tags_ogg(path, **kwargs):
+    oggfile = OggVorbis(path)
+    for tag, value in kwargs.items():
+        if not tag in EasyID3.valid_keys.keys():
+            continue
+        oggfile.tags[tag.upper()] = value
+
+    oggfile.save()
+
+def pick_and_apply_tags(path, all_data):
+    print('\n** {}'.format(path))
+    choice_template = '\t'.join(['({index})', 'ARTIST={artist}',
+                                 'TITLE={title}', 'ALBUM={album}', 'YEAR={year}'])
+    for index, ds in enumerate(all_data):
+        tags = ds['tracks'][0]
+        print(choice_template.format(index=index, artist=tags[1],
+                                     title=tags[0], album=tags[2], year=tags[3]))
+
+    invalid_reply = True
+    while invalid_reply:
+        reply = input('type number to pick suggestion, s to skip, q to quit. ').lower().strip()
+        print(reply)
+        if reply == 'q':
+            sys.exit()
+        elif reply == 's':
+            return
+        try:
+            index = int(reply)
+            tags = all_data[index]['tracks'][0]
+            apply_tags_mp3(path, artist=tags[0], title=tags[1], album=tags[2], year=tags[3])
+            invalid_reply = False
+        except (ValueError, IndexError):
+            invalid_reply = True
+
+
 def main():
-    p = truncate_audio_file('/home/jonas/Musik/72 Zum Laichen Und Sterben Ziehen Di.mp3')
+    testfile = '/home/jonas/Python/audioidentifier/72 Zum Laichen Und Sterben Ziehen Di.mp3'
+    p = truncate_audio_file(testfile)
     print(p)
     t = upload_for_identification(p, API_KEY)
-    retrieve_result(t, API_KEY)
+    all_data = retrieve_result(t, API_KEY)
+    pick_and_apply_tags(testfile, all_data)
 
 if __name__ == '__main__':
     main()
