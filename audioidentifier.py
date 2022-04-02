508a98504634d91d448880717457db526aeddc85
Jonas Neundorf <jneun1@web.de>
2022-04-02 21:21:30 +0200

    add license

--- audioidentifier.py
+++ audioidentifier.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
+# Copyright 2022 Jonas Neundorf
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
 
 import argparse
 import os
