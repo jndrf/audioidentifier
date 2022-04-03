b110ff7ce25b1a2d758b1b9baac2d809ae928e4e
Jonas Neundorf <jneun1@web.de>
2022-04-03 22:00:03 +0200

    add tool to batch-rename files

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ Audio Identifier
 Uploads an audio snippet to [audiotag.info](https://audiotag.info/).
 If it can be identified, it shows the user the identified sets of tags (artist, title, album and year) and asks to choose one.
 The chosen set of tags is then directly applied to the file.
+A second tool takes care of renaming audio files according to their tags.
 
 Dependencies
 ------------
@@ -31,6 +32,21 @@ You need to create a free account at [audiotag.info](https://audiotag.info/) and
 This API key then has to be stored in a text file that is passed as argument to `audioidentifier.py`.
 I recommend to name the file `apikey.txt`, since this file name will be ignored by `git`.
 
+A second tool, `batchrename.py`, can then be used to rename the files based on their tracks.
+```
+% ./batchrename.py -husage: tool to batch-rename audio files [-h] [-p PATTERN] files [files ...]
+
+positional arguments:
+  files                 files to rename
+
+options:
+  -h, --help            show this help message and exit
+  -p PATTERN, --pattern PATTERN
+                        Pattern for the new file names using lower-case tag names and the syntax of
+                        str.format Default is '{artist}/{album}/{tracknumber}_{title}'. The file
+                        extension will be copied automatically, folders will be created as needed.
+```
+
 Feature Ideas
 -------------
 
@@ -39,3 +55,4 @@ Some ideas, in no particular order, are
 - support a different backend, e.g. [ffmpeg](https://www.ffmpeg.org).
 - make the requests async.
 - show tags of all files before deciding which to apply.
+- support more formats than ogg vorbis and mp3.
---
+++ batchrename.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# Copyright 2022 Jonas Neundorf
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+import argparse
+import os
+
+from mutagen.easyid3 import EasyID3
+from mutagen.oggvorbis import OggVorbis
+
+retrieve_tags = {'ogg':lambda path: OggVorbis(path).tags,
+                 'mp3': EasyID3}
+
+def flatten_tags(tags):
+    '''mutagen returns a list for each tag, take only first element of it'''
+    ret = {}
+    for key, taglist in tags.items():
+        tag = taglist[0].replace('/', '_')
+        ret[key] = tag
+    return ret
+
+def rename(inputfile, pattern):
+    extension = inputfile.split('.')[-1].lower()
+    tags = retrieve_tags[extension](inputfile)
+    tags = flatten_tags(tags)
+    tags['extension'] = extension
+    pattern = pattern + '.{extension}'
+
+    outputfile = pattern.format(**tags)
+
+    os.renames(inputfile, outputfile)
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser('tool to batch-rename audio files')
+    parser.add_argument('files', help='files to rename', nargs='+')
+    parser.add_argument('-p', '--pattern', default='{artist}/{album}/{tracknumber}_{title}',
+                        help='''
+Pattern for the new file names using lower-case tag names and the syntax of str.format
+Default is '{artist}/{album}/{tracknumber}_{title}'.
+The file extension will be copied automatically, folders will be created as needed.
+''')
+
+    args = parser.parse_args()
+
+    for f in args.files:
+        rename(f, args.pattern)
